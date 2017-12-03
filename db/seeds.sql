70541d642fd9c88b753eb30ba750b8b2fd068714
jakadamko <jakadamko@gmail.com>
2017-12-02 19:19:43 -0500

    12/2/2017

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Big Mac", True);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Whopper", True);
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Cheeseburger", True);
+
+
+SELECT * FROM burgers;
